a3cfc476d7227fc67ed5daf48bd32f5e069fb847
Ximena Gutierrez Perez <gutierrezperezximena4@gmail.com>
2025-11-12 15:04:57 -0600

    JAIRO -> Configuracion adicional y ABC

--- senas.py
+++ senas.py
@@ -26,6 +26,12 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+# Sensibilidad a la distancia en píxeles. Ajusta estos valores si la detección es inestable.
+DELTA_Y = 50 
+DELTA_DIST_CLOSE = 40 # Distancia para considerar dedos juntos/doblados
+DELTA_DIST_FAR = 100 # Distancia para considerar dedos separados
+
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 1920)
 cap.set(4, 1080)
@@ -47,94 +53,68 @@ with mp_hands.Hands(
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         image_height, image_width, _ = image.shape
+        predicted_letter = "" 
+
         if results.multi_hand_landmarks:
-            if len(results.multi_hand_landmarks):
-                for num, hand_landmarks in enumerate(results.multi_hand_landmarks):
+            for num, hand_landmarks in enumerate(results.multi_hand_landmarks):
                     
-                    mp_drawing.draw_landmarks(
-                        image,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style())
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
                     
                     # Draw bounding box
-                    draw_bounding_box(image, hand_landmarks)
+                draw_bounding_box(image, hand_landmarks)
 
-                    # Obtener coordenadas de los puntos clave
-                    index_finger_tip = (int(hand_landmarks.landmark[8].x * image_width),
-                                        int(hand_landmarks.landmark[8].y * image_height))
-                    index_finger_pip = (int(hand_landmarks.landmark[6].x * image_width),
-                                        int(hand_landmarks.landmark[6].y * image_height))
-                    
-                    thumb_tip = (int(hand_landmarks.landmark[4].x * image_width),
-                                 int(hand_landmarks.landmark[4].y * image_height))
-                    thumb_pip = (int(hand_landmarks.landmark[2].x * image_width),
-                                 int(hand_landmarks.landmark[2].y * image_height))
-                    
-                    middle_finger_tip = (int(hand_landmarks.landmark[12].x * image_width),
-                                         int(hand_landmarks.landmark[12].y * image_height))
-                    middle_finger_pip = (int(hand_landmarks.landmark[10].x * image_width),
-                                         int(hand_landmarks.landmark[10].y * image_height))
-                    
-                    ring_finger_tip = (int(hand_landmarks.landmark[16].x * image_width),
-                                         int(hand_landmarks.landmark[16].y * image_height))
-                    ring_finger_pip = (int(hand_landmarks.landmark[14].x * image_width),
-                                         int(hand_landmarks.landmark[14].y * image_height))
-                    
-                    pinky_tip = (int(hand_landmarks.landmark[20].x * image_width),
-                                 int(hand_landmarks.landmark[20].y * image_height))
-                    pinky_pip = (int(hand_landmarks.landmark[18].x * image_width),
-                                 int(hand_landmarks.landmark[18].y * image_height))
-                    
-                    wrist = (int(hand_landmarks.landmark[0].x * image_width),
-                             int(hand_landmarks.landmark[0].y * image_height))
-                    
-                    ring_finger_mcp = (int(hand_landmarks.landmark[13].x * image_width),
-                                      int(hand_landmarks.landmark[13].y * image_height))
+                # PUNTAS (TIP)
+                thumb_tip = (int(hand_landmarks.landmark[4].x * image_width), int(hand_landmarks.landmark[4].y * image_height))
+                index_tip = (int(hand_landmarks.landmark[8].x * image_width), int(hand_landmarks.landmark[8].y * image_height))
+                middle_tip = (int(hand_landmarks.landmark[12].x * image_width), int(hand_landmarks.landmark[12].y * image_height))
+                ring_tip = (int(hand_landmarks.landmark[16].x * image_width), int(hand_landmarks.landmark[16].y * image_height))
+                pinky_tip = (int(hand_landmarks.landmark[20].x * image_width), int(hand_landmarks.landmark[20].y * image_height))
+                
+                # FALANGES MEDIAS (PIP)
+                thumb_pip = (int(hand_landmarks.landmark[2].x * image_width), int(hand_landmarks.landmark[2].y * image_height))
+                index_pip = (int(hand_landmarks.landmark[6].x * image_width), int(hand_landmarks.landmark[6].y * image_height))
+                middle_pip = (int(hand_landmarks.landmark[10].x * image_width), int(hand_landmarks.landmark[10].y * image_height))
+                ring_pip = (int(hand_landmarks.landmark[14].x * image_width), int(hand_landmarks.landmark[14].y * image_height))
+                pinky_pip = (int(hand_landmarks.landmark[18].x * image_width), int(hand_landmarks.landmark[18].y * image_height))
 
+                # OTROS PUNTOS
+                wrist = (int(hand_landmarks.landmark[0].x * image_width), int(hand_landmarks.landmark[0].y * image_height))
+                
+                is_index_extended = index_tip[1] < index_pip[1] - DELTA_Y
+                is_middle_extended = middle_tip[1] < middle_pip[1] - DELTA_Y
+                is_ring_extended = ring_tip[1] < ring_pip[1] - DELTA_Y
+                is_pinky_extended = pinky_tip[1] < pinky_pip[1] - DELTA_Y
+                
+                is_index_folded = index_tip[1] > index_pip[1] + DELTA_Y
+                is_middle_folded = middle_tip[1] > middle_pip[1] + DELTA_Y
+                is_ring_folded = ring_tip[1] > ring_pip[1] + DELTA_Y
+                is_pinky_folded = pinky_tip[1] > pinky_pip[1] + DELTA_Y
+                
+                # Lógica del pulgar (más difícil por la rotación)
+                is_thumb_folded = thumb_tip[1] > thumb_pip[1] + 30 or distancia_euclidiana(thumb_tip, thumb_pip) < 50
+                is_thumb_separated = distancia_euclidiana(thumb_tip, index_tip) > DELTA_DIST_FAR
 
-                        # A - Puño cerrado con el pulgar a un lado.
-                    if abs(thumb_tip[1] - index_finger_pip[1]) < 45 \
-                        and abs(thumb_tip[1] - middle_finger_pip[1]) < 30 and abs(thumb_tip[1] - ring_finger_pip[1]) < 30 \
-                        and abs(thumb_tip[1] - pinky_pip[1]) < 30:
-                        cv2.putText(image, 'A', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
-                        
-                         # E - Puño cerrado con las puntas de los dedos tocando la palma.
-                    elif index_finger_pip[1] - index_finger_tip[1] < 0 and pinky_pip[1] - pinky_tip[1] < 0 and \
-                        middle_finger_pip[1] - middle_finger_tip[1] < 0 and ring_finger_pip[1] - ring_finger_tip[1] < 0 \
-                        and abs(index_finger_tip[1] - thumb_tip[1]) < 100:
-                        # Se ha mantenido la lógica original para 'E'.
-                        cv2.putText(image, 'E', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
-                        
-                        # I - Meñique extendido, el resto doblado.
-                    elif distancia_euclidiana(pinky_tip, wrist) > distancia_euclidiana(ring_finger_tip, wrist) and \
-                         pinky_pip[1] - pinky_tip[1] > 0 and \
-                         distancia_euclidiana(thumb_tip, index_finger_tip) > 100: # Pulgar separado
-                        cv2.putText(image, 'I', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
+                # A: Puño cerrado, pulgar a un lado.
+                if is_index_folded and is_middle_folded and is_ring_folded and is_pinky_folded and \
+                   thumb_tip[1] < index_pip[1] and thumb_tip[0] > index_pip[0]: 
+                    predicted_letter = 'A'
 
-                    # O - Dedos curvados y unidos formando un círculo.
-                    elif distancia_euclidiana(thumb_tip, index_finger_tip) < 55 and \
-                         distancia_euclidiana(thumb_tip, middle_finger_tip) < 55 and \
-                         distancia_euclidiana(index_finger_pip, ring_finger_mcp) > 80: # Abre la mano ligeramente y une las puntas
-                        cv2.putText(image, 'O', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
-                        
-                     # U - Índice y medio extendidos y juntos, el resto doblado.
-                    elif index_finger_pip[1] - index_finger_tip[1] > 0 and middle_finger_pip[1] - middle_finger_tip[1] > 0 and \
-                         abs(index_finger_tip[0] - middle_finger_tip[0]) < 30 and \
-                         pinky_pip[1] - pinky_tip[1] < 0 and ring_finger_pip[1] - ring_finger_tip[1] < 0 and \
-                         thumb_tip[1] - thumb_pip[1] < 0: # Dedo anular y meñique doblados, pulgar pegado
-                        cv2.putText(image, 'U', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
+                # B: Mano abierta y extendida, pulgar doblado (o cerca de la palma).
+                elif is_index_extended and is_middle_extended and is_ring_extended and is_pinky_extended and \
+                     not is_thumb_separated: 
+                    predicted_letter = 'B'
+
+                # C: Mano curvada, como si sostuviera una taza.
+                elif not is_index_extended and not is_pinky_extended and \
+                     distancia_euclidiana(index_tip, wrist) > DELTA_DIST_FAR and \
+                     distancia_euclidiana(pinky_tip, wrist) > DELTA_DIST_FAR and \
+                     distancia_euclidiana(index_tip, middle_tip) < DELTA_DIST_CLOSE * 1.5: 
+                    predicted_letter = 'C'
                         
         cv2.imshow('MediaPipe Hands - Vocales (ASL)', image)
         if cv2.waitKey(5) & 0xFF == 27:
